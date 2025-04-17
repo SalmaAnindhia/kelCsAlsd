3a701db81c488697a80ff7e96923d5fd18eb5d51
Salmaanindhia <salmanuranindhia@gamil.com>
2025-04-17 10:58:50 +0700

    penilaian26

--- Penilaian.java
+++ Penilaian.java
@@ -12,9 +12,22 @@ public class Penilaian{
         this.nilaiTugas = nilaiTugas;
         this.nilaiUts = nilaiUts;
         this.nilaiUas = nilaiUas;
+        hitungNilaiAkhir();
     }
 
-    void hitungNilaiakhir(){
+    void hitungNilaiAkhir(){
         nilaiAkhir = (nilaiTugas * 0.3) + (nilaiUts * 0.3) + (nilaiUas * 0.4);
     }
+
+    void tampilPenilaian() {
+        System.out.println("Mahasiswa: " + mahasiswa.nama);
+        System.out.println("Mata Kuliah: " + matakuliah.NamaMK);
+        System.out.println("Nilai Tugas: " + nilaiTugas);
+        System.out.println("Nilai UTS: " + nilaiUts);
+        System.out.println("Nilai UAS: " + nilaiUas);
+        hitungNilaiAkhir(); 
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        System.out.println("------------------------");
+    }
+    
 }
